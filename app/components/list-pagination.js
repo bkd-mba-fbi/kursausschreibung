0bfc66a21b3bb3ffe4f65a6e90c0a1b1390f8184
Michael Schertenleib <michaelschertenleib@gmail.com>
2018-03-15 19:29:21 +0100

    pagination and search

---
+++ app/components/list-pagination.js
@@ -0,0 +1,68 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+// pages to show before and after the current page
+let n = 2;
+
+// number of items per page
+let itemsPerPage = 5;
+
+export default Component.extend({
+  lastPage: computed('items', function () {
+    return Math.ceil(this.get('items').length / itemsPerPage);
+  }),
+
+  isFirstPage: computed('page', function () {
+    return this.get('page') === 1;
+  }),
+
+  isLastPage: computed('page', 'lastPage', function () {
+    return this.get('page') === this.get('lastPage');
+  }),
+
+  nextPage: computed('page', function () {
+    return this.get('page') + 1;
+  }),
+
+  previousPage: computed('page', function () {
+    return this.get('page') - 1;
+  }),
+
+  showFirst: computed('page', function () {
+    return this.get('page') > 1 + n;
+  }),
+
+  showLast: computed('page', 'lastPage', function () {
+    return this.get('page') < this.get('lastPage') - n;
+  }),
+
+  showLeftDots: computed('page', function () {
+    return this.get('page') > n + 2;
+  }),
+
+  showRightDots: computed('page', 'lastPage', function () {
+    return this.get('page') < this.get('lastPage') - (n + 1);
+  }),
+
+  pages: computed('page', 'lastPage', function () {
+    let page = this.get('page');
+    let lastPage = this.get('lastPage');
+
+    let min = page - n >= 1 ? page - n : 1;
+    let max = page + n <= lastPage ? page + n : lastPage;
+
+    let pages = [];
+
+    for (let i = min; i <= max; i++) {
+      pages.push({ page: i, active: i === page });
+    }
+
+    return pages;
+  }),
+
+  itemsOnCurrentPage: computed('items', 'page', function () {
+    let page = this.get('page');
+
+    return this.get('items').slice(itemsPerPage * (page - 1), itemsPerPage * page);
+  })
+});
--- app/controllers/list.js
+++ app/controllers/list.js
@@ -1,14 +1,22 @@
 import Controller from '@ember/controller';
-import { computed } from "@ember/object";
+import { computed, observer } from "@ember/object";
 
 export default Controller.extend({
-  searchString: '',
+  query: '',
+  page: 1,
 
-  events: computed('model', 'searchString', function () {
-    let searchString = this.get('searchString');
+  events: computed('model', 'query', function () {
+    let query = this.get('query');
     return this.get('model').filter((event) =>
       event.Designation.toLowerCase()
-        .indexOf(this.get('searchString').toLowerCase()) !== -1
+        .indexOf(this.get('query').toLowerCase()) !== -1
     );
-  })
+  }),
+
+  // reset page when searching
+  queryChanged: observer('query', function() {
+    this.set('page', 1);
+  }),
+
+  queryParams: ['query', 'page']
 });
