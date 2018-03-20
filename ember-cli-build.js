9fac8f168176cd6ae42cef701243d4c22298c6fa
Michael Schertenleib <michaelschertenleib@gmail.com>
2018-03-20 15:43:51 +0100

    exclude appConfig from minification

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,9 +2,13 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+
+    // see: https://github.com/ember-cli/ember-cli-uglify
+    'ember-cli-uglify': {
+      exclude: ['appConfig.js']
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
