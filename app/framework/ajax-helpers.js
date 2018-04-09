6555bfbe2b45768a97802a58d611a1e1e9396518
Michael Schertenleib <michaelschertenleib@gmail.com>
2018-04-09 15:35:40 +0200

    use $.getJSON instead of $.get

--- app/framework/ajax-helpers.js
+++ app/framework/ajax-helpers.js
@@ -1,13 +1,11 @@
 import $ from 'jquery';
-import { Promise } from 'rsvp';
 
 // fetches JSON-file with `url` from server and merges
 // its values with `object`
 // (also removes comments)
 export function fetchJSON(url, object) {
-  return $.get({
+  return $.getJSON({
     url,
-    dataType: 'json',
 
     dataFilter(data) {
       // remove comments
