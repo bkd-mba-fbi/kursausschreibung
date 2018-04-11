80ada1f44a06f25658b67d0e2a4d1e2064058ccc
Michael Schertenleib <michaelschertenleib@gmail.com>
2018-04-11 16:41:38 +0200

    add mobile-menu

--- app/routes/list.js
+++ app/routes/list.js
@@ -1,15 +1,26 @@
 import Route from '@ember/routing/route';
+import uikit from 'uikit';
 
 export default Route.extend({
   model(params) {
     let eventsByArea = this.modelFor('application').get('eventsByArea');
 
     // check if area of education exists
-    if (!(eventsByArea.hasOwnProperty(params.area_of_education))) {
+    if (!eventsByArea.hasOwnProperty(params.area_of_education)) {
       this.replaceWith('/');
       return;
     }
 
     return eventsByArea[params.area_of_education];
+  },
+
+  actions: {
+    didTransition() {
+      let modal = uikit.modal('#menu-modal');
+
+      if (modal !== undefined) {
+        modal.hide();
+      }
+    }
   }
 });
