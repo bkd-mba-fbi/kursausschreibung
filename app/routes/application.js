9ed35c70c50e5bb2d0eea191f2a5f0c079e17463
Michael Schertenleib <michaelschertenleib@gmail.com>
2018-04-16 16:43:31 +0200

    add uk-scope (run `npm run scope` to create uikit build)

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,9 +4,13 @@ import { init as appConfigInit } from '../framework/app-config';
 import { init as translateInit } from '../framework/translate';
 import { init as storeInit, getAllEvents } from '../framework/store';
 import { all } from 'rsvp';
+import uikit from 'uikit';
 
 export default Route.extend({
   beforeModel() {
+    // set uikit scope
+    uikit.container = '.uk-scope';
+
     // initialize appconfig, config and translation
     // this is loosely based on
     // https://github.com/emberjs/ember.js/issues/11247#issuecomment-118143934
