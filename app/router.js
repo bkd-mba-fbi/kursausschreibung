1f76bf4bc60c6121cf8fac6cde045675f9ab1c65
Michael Schertenleib <michaelschertenleib@gmail.com>
2018-04-24 16:56:15 +0200

    reset scroll position on transition
    taken from https://romulomachado.github.io/2016/12/20/resetting-scroll-on-route-changes.html

--- app/router.js
+++ app/router.js
@@ -3,13 +3,18 @@ import config from './config/environment';
 
 const Router = EmberRouter.extend({
   location: config.locationType,
-  rootURL: config.rootURL
+  rootURL: config.rootURL,
+
+  didTransition() {
+    this._super(...arguments);
+    window.scrollTo(0, 0);
+  }
 });
 
 Router.map(function() {
-  this.route('list', {path: '/:area_of_education'}, function() {
-    this.route('category', {path: '/:category'}, function() {
-      this.route('event', {path: '/:event_id'}, function() {
+  this.route('list', { path: '/:area_of_education' }, function() {
+    this.route('category', { path: '/:category' }, function() {
+      this.route('event', { path: '/:event_id' }, function() {
         this.route('subscribe');
         this.route('confirmation');
       });
