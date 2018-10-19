01b6e9cb3aa08b1d005fe5bbafa8d43aed312d37
Michael Schertenleib <michaelschertenleib@gmail.com>
2018-10-19 16:57:56 +0200

    reset ENV.APP.rootElement to #ember-testing

--- config/environment.js
+++ config/environment.js
@@ -39,6 +39,8 @@ module.exports = function(environment) {
     // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
     ENV.APP.autoboot = false;
   }
 
