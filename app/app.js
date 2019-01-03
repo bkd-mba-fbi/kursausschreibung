3c13d8bccb65f73d16ef9ef2b9600f86c1318c2d
Michael Schertenleib <michaelschertenleib@gmail.com>
2019-01-03 06:14:28 +0100

    use jQuery.noConflict() (see #13)

--- app/app.js
+++ app/app.js
@@ -1,9 +1,12 @@
+import $ from 'jquery';
 import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 import { checkToken } from 'kursausschreibung/framework/login-helpers';
 
+// restore window.$ and window.jQuery
+$.noConflict(true);
 
 // read OAuth token and restore URL
 checkToken();
