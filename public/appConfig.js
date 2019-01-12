310246f35f4127260467083aafe0a992754e0fc3
Urs Bräm <ub@sturmundbraem.ch>
2019-01-12 16:01:26 +0100

    Update appconfig

--- public/appConfig.js
+++ public/appConfig.js
@@ -1,19 +1,28 @@
 window.kursausschreibung = window.kursausschreibung || {};
 window.kursausschreibung.appConfig = {
-  // the token type
-  "tokenType": "urn:ietf:params:oauth:token-type:jwt-bearer",
-  // API base URL without trailing slash
-  "apiUrl": "https://eventotest.api",
-  // base Url of the web application without trailing slash (also the redirect url for public clients)
-  "webBaseUrl": "https://test.com",
-  // base url of the CLX.Evento OAuth Server.
-  "oauthUrl": "https://eventotest.oauth",
-  // the instance id for this application.
-  "instanceId": "testInstanz",
-  // the client id for this application.
-  "clientId": "testClientId",
-  // set this to true if you want to use autologin on your page. If "false" the authorization must be done by the application, that module does not perform authorization. If false and no token found in the local storage (CLX.LoginToken) the "kursausschreibung" application only refresh the actual url. The application must be redirect user to login form.
-  "useAutoLogin": true,
-  // the scope of the application
-  "applicationScope": "Public"
+    // the token type
+    "tokenType": "urn:ietf:params:oauth:token-type:jwt-bearer",
+    // API base URL without trailing slash
+    "apiUrl": "https://eventoapp-test.erz.be.ch/restApi", // "https://b17eb32d-b72d-4238-a677-74639b5dbf20.mock.pstmn.io"
+    // base Url of the web application without trailing slash (also the redirect url for public clients)
+    "webBaseUrl": "http://localhost",
+    // url to a page that handles login without trailing slash. If there is no login page on the website, this
+    // must be the url of the login form in CLX.Evento OAuth Server: https://{BaseUriOAuth}/Authorization/{InstanceId}/Login
+    "loginUrl": null,
+    // base url of the CLX.Evento OAuth Server. Provide this value if you are intending to use CLX.Evnto OAuth Server
+    // for authentication. In this case you MUST leave "loginUrl" empty
+    "oauthUrl": "https://eventoapp-test.erz.be.ch/OAuth",
+    // the instance id for this application. This value is mandatory when authenticate with CLX.Evento OAuth Server
+    "instanceId": "BsBFF",
+    // the client id for this application. This value is mandatory when authenticate with CLX.Evento OAuth Server
+    "clientId": "dev",
+    // url to a page that can refresh the session (website and modules)
+    "refreshSessionUrl": "",
+    // the interval to refresh the session on website and modules (in minutes)
+    "refreshInterval": 5,
+    // set this to true if you want to use autologin on your page
+    "useAutoLogin": true,
+    // set this to true if your webserver is not detecting the language, or if you can't add the language to the <html> element (HTML5)
+    "useLanguageDetection": true,
+    "applicationScope": "Public"
 };
