eb2532e6a781cd4d1488bb14d69386c4c30e3034
schefbi <sandro.scheidt@be.ch>
2021-09-09 17:08:53 +0200

    #117 icon status lamp

--- app/components/status-lamp.js
+++ app/components/status-lamp.js
@@ -3,11 +3,11 @@ import { observer } from '@ember/object';
 import { getString } from 'kursausschreibung/framework/translate';
 
 const statuses = {
-  green: { tooltip: getString('greenLamp'), className: 'lamp-green' },
-  chartreuse: { tooltip: getString('chartreuseLamp'), className: 'lamp-chartreuse' },
-  yellow: { tooltip: getString('yellowLamp'), className: 'lamp-yellow' },
-  red: { tooltip: getString('redLamp'), className: 'lamp-red' },
-  orange: { tooltip: getString('orangeLamp'), className: 'lamp-orange' }
+  green: { tooltip: getString('greenLamp'), className: 'lamp-green', icon: 'pencil' },
+  chartreuse: { tooltip: getString('chartreuseLamp'), className: 'lamp-chartreuse', icon: 'check' },
+  yellow: { tooltip: getString('yellowLamp'), className: 'lamp-yellow', icon: 'clock' },
+  red: { tooltip: getString('redLamp'), className: 'lamp-red', icon: 'close' },
+  orange: { tooltip: getString('orangeLamp'), className: 'lamp-orange', icon: 'ban' }
 };
 
 export default Component.extend({
@@ -23,11 +23,12 @@ export default Component.extend({
     if (status !== undefined) {
       this.set('tooltip', status.tooltip);
       this.set('color', status.className);
+      this.set('icon', status.icon);
     }
   }),
 
   tagName: 'span',
-  attributeBindings: ['tooltip:data-uk-tooltip'],
-  classNames: ['status-lamp'],
+  attributeBindings: ['tooltip:data-uk-tooltip',"icon:uk-icon"],
+  classNames: ['status-lamp','icon-lamp'],
   classNameBindings: ['color']
 });
