46ad3f5d01999de28c0739724b18c59f297e4baa
Sandro <41326409+schefbi@users.noreply.github.com>
2021-12-09 15:51:12 +0100

    #134 bug fix

--- app/framework/date-helpers.js
+++ app/framework/date-helpers.js
@@ -85,10 +85,14 @@ export function eventStarted(event) {
  */
 export function eventEnded(event) {
   let now = new Date();
-  if (event.DateTo === null) {
+  let dateTo = event.DateTo;
+  let repalcePattern = '00:00:00';
+  if (dateTo === null) {
     return false;
   }
-  return parseISO(event.DateTo).getTime() <= now.getTime();
+
+  dateTo = dateTo.search(repalcePattern) > 0 ? dateTo.replace(repalcePattern,event.TimeTo) : dateTo;
+  return parseISO(dateTo).getTime() <= now.getTime();
 }
 
 /**
