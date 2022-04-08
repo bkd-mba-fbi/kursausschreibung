db0f5eaa3afed6a517cd3320230b4c0cc4f971c3
Sandro <41326409+schefbi@users.noreply.github.com>
2022-04-08 07:57:08 +0200

    #147 local files

--- public/locale/de-CH.js
+++ public/locale/de-CH.js
@@ -41,7 +41,7 @@ window.kursausschreibung.locale["de-CH"] = {
   "subscriptionDetailFields": "Anmeldedetails",
   "sendingData": "Daten senden",
   "subscriptionFailed": "Anmeldung fehlgeschlagen.",
-  "subscriptionFilesUploadFailed": "Die Anmeldung war erfolgreich. Es konnten jedoch nicht alle Dateien an den Server übermittelt werden. Bitte melden Sie sich direkt bei...",
+  "subscriptionFilesUploadFailed": " Die Anmeldung war erfolgreich. Es konnten jedoch nicht alle Dateien übermittelt werden. Bitte melden Sie sich beim Veranstalter.",
   "noEvents": "Es ist momentan keine Veranstaltung ausgeschrieben.",
   "FilterTagAllEvents": "Alle",
   "FilterTag1018": "Test 1",
