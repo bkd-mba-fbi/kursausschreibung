53f91e87ba8bf9116af6d77c7d14938dbc4b25ed
Sandro <sandro.scheidt@be.ch>
2024-08-15 12:07:28 +0200

    didTransition To routeDidChange

--- app/controllers/list/category/event/index.js
+++ app/controllers/list/category/event/index.js
@@ -1,15 +1,9 @@
 import Controller from '@ember/controller';
 import settings from 'kursausschreibung/framework/settings';
-import LinkComponent from '@ember/routing/link-component';
 
 let badgeFreeSeatsEnabled = typeof settings.badgeFreeSeats === 'object' && settings.badgeFreeSeats.enabled === true;
 
 export default Controller.extend({
   showBreadcrumbs: settings.showBreadcrumbs,
   badgeFreeSeatsEnabled
-});
-
-// bindings for tooltip and disabled attributes
-LinkComponent.reopen({
-  attributeBindings: ['data-uk-tooltip', 'disabled']
 });
