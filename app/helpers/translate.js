364c33aed2a0671e82591fd260dd5a3466affa63
Kai Boschung <kai@engineerer.ch>
2026-04-10 13:57:18 +0200

    refactor(ember4): batch 1 normalize translate helper args in templates

--- app/helpers/translate.js
+++ app/helpers/translate.js
@@ -2,8 +2,9 @@ import { helper } from '@ember/component/helper';
 import { getString } from 'kursausschreibung/framework/translate';
 import { htmlSafe } from '@ember/template';
 
-export function translate([key, ...placeholderValues]) {
-  return htmlSafe(getString(key, placeholderValues));
+export function translate([positionalKey, ...placeholderValues], { key } = {}) {
+  const translationKey = key ?? positionalKey;
+  return htmlSafe(getString(translationKey, placeholderValues));
 }
 
 export default helper(translate);
