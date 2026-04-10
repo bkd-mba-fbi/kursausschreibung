a03de7f6cd653e46da184f1796c2f66b6f8c5f34
Kai Boschung <kai@engineerer.ch>
2026-04-10 22:12:55 +0200

    refactor(ember4): convert input-file to a Glimmer component

--- app/components/input/input-file.js
+++ app/components/input/input-file.js
@@ -1,4 +1,4 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { getString } from 'kursausschreibung/framework/translate';
 import {
@@ -18,30 +18,30 @@ function getElementIdFile(fieldId) {
 }
 
 export default class InputFileComponent extends Component {
-  change() {
-    let elementIdFile = getElementIdFile(this.field.id);
-    let inputFile = getInputFile(this.field.id);
+  @action
+  handleChange() {
+    let field = this.args.field;
+    let elementIdFile = getElementIdFile(field.id);
+    let inputFile = getInputFile(field.id);
     inputFile.imgDev = null;
-    let maxFileSizeMB = (this.field.maxFileSize / (1024 * 1024)).toFixed(2);
+    let maxFileSizeMB = (field.maxFileSize / (1024 * 1024)).toFixed(2);
 
-    if (inputFile.size > this.field.maxFileSize && maxFileSizeMB !== '0.00') {
+    if (inputFile.size > field.maxFileSize && maxFileSizeMB !== '0.00') {
       uikit.modal.alert(getString('FileSizeTooBig') + maxFileSizeMB + 'MB');
       removeFile(elementIdFile);
     } else if (
-      this.field.acceptFileType.indexOf(inputFile.type) === -1 ||
+      field.acceptFileType.indexOf(inputFile.type) === -1 ||
       inputFile.type === ''
     ) {
-      uikit.modal.alert(
-        getString('FileTypeNotAccept') + this.field.acceptFileType
-      );
+      uikit.modal.alert(getString('FileTypeNotAccept') + field.acceptFileType);
       removeFile(elementIdFile);
     } else {
-      this.field.fileTypeLabel = inputFile.name;
-      this.field.fileObject = inputFile;
+      field.fileTypeLabel = inputFile.name;
+      field.fileObject = inputFile;
 
-      let buttonClass = document.getElementById('fileBt' + this.field.id);
+      let buttonClass = document.getElementById('fileBt' + field.id);
       buttonClass.classList.remove('required');
-      let buttonClassDel = document.getElementById('fileBtDel' + this.field.id);
+      let buttonClassDel = document.getElementById('fileBtDel' + field.id);
       buttonClassDel.classList.remove('uk-hidden');
 
       const reader = new FileReader();
@@ -50,15 +50,15 @@ export default class InputFileComponent extends Component {
       // Note: reading file is async
       reader.onload = () => {
         data = reader.result;
-        this.field.fileObject.data = data;
+        field.fileObject.data = data;
       };
 
       if (inputFile) {
         reader.readAsDataURL(inputFile);
       }
 
-      if (this.field.acceptFileType === 'image/jpeg') {
-        let fieldId = this.field.id;
+      if (field.acceptFileType === 'image/jpeg') {
+        let fieldId = field.id;
         let buttonClassUpload = document.getElementById(
           'fileBtUpload' + fieldId
         );
@@ -67,7 +67,7 @@ export default class InputFileComponent extends Component {
         let imgField = document.getElementById('img' + fieldId);
         imgField.classList.remove('uk-hidden');
 
-        var basic = jQuery('#img' + this.field.id).croppie({
+        var basic = jQuery('#img' + field.id).croppie({
           viewport: { width: 300, height: 400 },
           boundary: { width: 350, height: 450 },
         });
@@ -83,20 +83,20 @@ export default class InputFileComponent extends Component {
         status: 'success',
       });
 
-      let field = this.field;
       vssDependency(inputFile, field);
     }
   }
 
   @action
   deleteFile() {
-    let fieldId = this.field.id;
+    let field = this.args.field;
+    let fieldId = field.id;
     let elementIdFile = getElementIdFile(fieldId);
     let buttonClassDel = document.getElementById('fileBtDel' + fieldId);
     buttonClassDel.classList.add('uk-hidden');
 
-    if (this.field.options?.required) {
-      let buttonClass = document.getElementById('fileBt' + this.field.id);
+    if (field.options?.required) {
+      let buttonClass = document.getElementById('fileBt' + field.id);
       buttonClass.classList.add('required');
     }
 
@@ -107,14 +107,14 @@ export default class InputFileComponent extends Component {
     let imgFielDev = document.getElementById('imgDev' + fieldId);
     imgFielDev.classList.add('uk-hidden');
     removeFile(elementIdFile);
-    this.field.fileTypeLabel = this.field.fileLabelBevorFileChoose;
+    field.fileTypeLabel = field.fileLabelBevorFileChoose;
 
-    jQuery('#img' + this.field.id).croppie('destroy');
+    jQuery('#img' + field.id).croppie('destroy');
   }
 
   @action
   uploadImage() {
-    let fieldId = this.field.id;
+    let fieldId = this.args.field.id;
     //on button click
     let basic = jQuery('#img' + fieldId);
     let inputFile = getInputFile(fieldId);
