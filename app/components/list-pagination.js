f68855efde0496d03582789ba94a120bfb536c3a
Kai Boschung <kai@engineerer.ch>
2026-04-10 15:38:13 +0200

    refactor(ember4): migrate list-pagination to native class getters/actions

--- app/components/list-pagination.js
+++ app/components/list-pagination.js
@@ -1,6 +1,5 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
-import { equal } from '@ember/object/computed';
+import { action } from '@ember/object';
 import settings from 'kursausschreibung/framework/settings';
 import { displayAsGrid } from 'kursausschreibung/framework/gui-helpers';
 import { getListViewGrid } from 'kursausschreibung/framework/storage';
@@ -8,45 +7,47 @@ import { getListViewGrid } from 'kursausschreibung/framework/storage';
 // pages to show before and after the current page
 let n = 2;
 
-export default Component.extend({
-  lastPage: computed('items.length', function () {
+export default class ListPaginationComponent extends Component {
+  get lastPage() {
     let filter = this.items.filter((item) => item.codes instanceof Array);
     return filter.length > 0
       ? 1
       : Math.ceil(this.items.length / settings.itemsPerPage);
-  }),
+  }
 
-  isFirstPage: equal('page', 1),
+  get isFirstPage() {
+    return this.page === 1;
+  }
 
-  isLastPage: computed('page', 'lastPage', function () {
+  get isLastPage() {
     return this.page === this.lastPage;
-  }),
+  }
 
-  nextPage: computed('page', function () {
+  get nextPage() {
     return this.page + 1;
-  }),
+  }
 
-  previousPage: computed('page', function () {
+  get previousPage() {
     return this.page - 1;
-  }),
+  }
 
-  showFirst: computed('page', function () {
+  get showFirst() {
     return this.page > 1 + n;
-  }),
+  }
 
-  showLast: computed('page', 'lastPage', function () {
+  get showLast() {
     return this.page < this.lastPage - n;
-  }),
+  }
 
-  showLeftDots: computed('page', function () {
+  get showLeftDots() {
     return this.page > n + 2;
-  }),
+  }
 
-  showRightDots: computed('page', 'lastPage', function () {
+  get showRightDots() {
     return this.page < this.lastPage - (n + 1);
-  }),
+  }
 
-  pages: computed('page', 'lastPage', function () {
+  get pages() {
     let page = this.page;
     let lastPage = this.lastPage;
 
@@ -60,9 +61,9 @@ export default Component.extend({
     }
 
     return pages;
-  }),
+  }
 
-  itemsOnCurrentPage: computed('items', 'page', function () {
+  get itemsOnCurrentPage() {
     let page = this.page;
     let filter = this.items.filter((item) => item.codes instanceof Array);
     return filter.length > 0
@@ -71,13 +72,18 @@ export default Component.extend({
           settings.itemsPerPage * (page - 1),
           settings.itemsPerPage * page
         );
-  }),
+  }
 
-  filterCodes: computed('items', 'itemsOnCurrentPage', function () {
+  get filterCodes() {
     let filterCodes = this.itemsOnCurrentPage.filter(
       (item) => item.allfilterCodes instanceof Array
     );
     let eventfilterCodes = [];
+
+    if (filterCodes.length === 0) {
+      return null;
+    }
+
     filterCodes.forEach((event) => {
       let existsFilter = filterCodes[0].allfilterCodes.filter(
         (filter) => event.filter.indexOf(filter.id) > -1
@@ -91,26 +97,29 @@ export default Component.extend({
     });
 
     return eventfilterCodes.length === 1 ? null : eventfilterCodes;
-  }),
+  }
 
-  actions: {
-    grid() {
-      displayAsGrid(true);
-    },
-    list() {
-      displayAsGrid(false);
-    },
-    cancelNav(e) {
-      e?.preventDefault?.();
-      e?.stopPropagation?.();
-      return false;
-    },
-  },
+  @action
+  grid() {
+    displayAsGrid(true);
+  }
+
+  @action
+  list() {
+    displayAsGrid(false);
+  }
+
+  @action
+  cancelNav(e) {
+    e?.preventDefault?.();
+    e?.stopPropagation?.();
+    return false;
+  }
 
   didRender() {
-    this._super(...arguments);
+    super.didRender(...arguments);
     var listViewGrid = getListViewGrid();
     listViewGrid = listViewGrid === null ? settings.displayGrid : listViewGrid;
     displayAsGrid(listViewGrid);
-  },
-});
+  }
+}
