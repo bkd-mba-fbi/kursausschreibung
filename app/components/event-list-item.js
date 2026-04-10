54136afa659a5ab076821a2934717e97c5c83b97
Kai Boschung <kai@engineerer.ch>
2026-04-10 15:10:13 +0200

    refactor(ember4): make event-list-item args usage explicit and simplify static fields

--- app/components/event-list-item.js
+++ app/components/event-list-item.js
@@ -3,14 +3,15 @@ import settings from 'kursausschreibung/framework/settings';
 import { getString } from 'kursausschreibung/framework/translate';
 import { camelize } from '@ember/string';
 
+const FIELDS = settings.eventListFields.map((key) => ({
+  name: getString(camelize(key)),
+  key,
+}));
+
 export default Component.extend({
   tagName: 'li',
   classNames: 'jsfilter',
 
   title: settings.eventListTitle,
-
-  fields: settings.eventListFields.map((key) => ({
-    name: getString(camelize(key)),
-    key,
-  })),
+  fields: FIELDS,
 });
