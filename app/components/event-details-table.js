4dde6773af93147caf5f936901a2edec1e30a8da
Kai Boschung <kai@engineerer.ch>
2026-04-10 15:07:09 +0200

    refactor(ember4): migrate event-details-table to Glimmer args/actions

--- app/components/event-details-table.js
+++ app/components/event-details-table.js
@@ -1,21 +1,23 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
 import settings from 'kursausschreibung/framework/settings';
 import { getString } from 'kursausschreibung/framework/translate';
 import { camelize } from '@ember/string';
 import { getIcsFileFromEvent } from 'kursausschreibung/framework/ics-file';
 
-export default Component.extend({
-  title: settings.eventDetailsTitle,
-  showEventText: settings.showEventText,
+const FIELDS = settings.eventDetailsFields.map((key) => ({
+  name: getString(camelize(key)),
+  key,
+}));
 
-  fields: settings.eventDetailsFields.map((key) => ({
-    name: getString(camelize(key)),
-    key,
-  })),
+export default class EventDetailsTableComponent extends Component {
+  title = settings.eventDetailsTitle;
+  showEventText = settings.showEventText;
+  fields = FIELDS;
 
-  actions: {
-    getIcsFileFromEvent() {
-      getIcsFileFromEvent(this.event);
-    },
-  },
-});
+  @action
+  downloadIcs(event) {
+    event.preventDefault();
+    getIcsFileFromEvent(this.args.event);
+  }
+}
