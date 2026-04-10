d31d10c92e669524ac40dc11983b60ce7bf191b5
Kai Boschung <kai@engineerer.ch>
2026-04-10 14:31:44 +0200

    refactor(ember4): batch 3 migrate remaining-seats-badge to Glimmer component

--- app/components/remaining-seats-badge.js
+++ app/components/remaining-seats-badge.js
@@ -1,14 +1,15 @@
-import Component from '@ember/component';
-import { computed } from '@ember/object';
+import Component from '@glimmer/component';
+import { registerDestructor } from '@ember/destroyable';
 import settings from 'kursausschreibung/framework/settings';
 
-export default Component.extend({
-  init() {
-    this._super(...arguments);
+export default class RemainingSeatsBadgeComponent extends Component {
+  intervalId;
 
-    let event = this.event;
+  constructor(owner, args) {
+    super(owner, args);
 
-    event.update();
+    const event = this.args.event;
+    event?.update?.();
 
     let interval =
       typeof settings.badgeFreeSeats === 'object'
@@ -22,24 +23,14 @@ export default Component.extend({
       interval = 30;
     }
 
-    // update freeSeats every <interval> seconds
-    this.set(
-      'interval',
-      setInterval(() => event.update(), interval * 1000)
-    );
-  },
+    this.intervalId = setInterval(() => event?.update?.(), interval * 1000);
+    registerDestructor(this, () => clearInterval(this.intervalId));
+  }
 
-  willDestroyElement() {
-    this._super(...arguments);
-    let interval = this.interval;
-
-    if (interval !== undefined) clearInterval(interval);
-  },
-
-  hidden: computed('event.{FreeSeats,status}', function () {
-    let freeSeats = this.get('event.FreeSeats');
-    let status = this.get('event.status');
-    let subscriptionYellowDisable =
+  get hidden() {
+    const freeSeats = this.args.event?.FreeSeats;
+    const status = this.args.event?.status;
+    const subscriptionYellowDisable =
       typeof settings.badgeFreeSeats === 'object'
         ? settings.badgeFreeSeats.subscriptionYellowDisable
         : false;
@@ -47,13 +38,13 @@ export default Component.extend({
     return (
       freeSeats === null || (subscriptionYellowDisable && status === 'yellow')
     );
-  }),
+  }
 
-  labelType: computed('event.FreeSeats', function () {
-    return this.get('event.FreeSeats') > 5 ? 'warning' : 'danger';
-  }),
+  get labelType() {
+    return this.args.event?.FreeSeats > 5 ? 'warning' : 'danger';
+  }
 
-  plural: computed('event.FreeSeats', function () {
-    return this.get('event.FreeSeats') !== 1;
-  }),
-});
+  get plural() {
+    return this.args.event?.FreeSeats !== 1;
+  }
+}
