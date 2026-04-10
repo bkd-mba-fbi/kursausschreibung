e1524b47bc8b92bd44c1e6bab3f59a1b97773ddb
Kai Boschung <kai@engineerer.ch>
2026-04-10 14:39:01 +0200

    refactor(ember4): replace input-email jQuery usage with native DOM APIs

--- app/components/input/input-email.js
+++ app/components/input/input-email.js
@@ -1,16 +1,17 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { formFieldError } from 'kursausschreibung/framework/form-helpers';
-import jQuery from 'jquery';
 
 export default class InputEmailComponent extends Component {
   @action
   handleChange() {
     // show an error message for duplicate e-mails
-    const emailFields = jQuery('#subscriptionForm')
-      .closest('form')
-      .find('input[type="email"]')
-      .toArray();
+    const form = document.getElementById('subscriptionForm')?.closest('form');
+    if (!form) {
+      return;
+    }
+
+    const emailFields = Array.from(form.querySelectorAll('input[type="email"]'));
     const emailFieldValues = emailFields.map((field) => field.value);
 
     emailFields.forEach((field, fieldIndex) => {
